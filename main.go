42441aafc135da3f207c1ae06d61b5a4c7fb9c91
Taavi Väänänen <hi@taavi.wtf>
2023-02-20 16:13:42 +0200

    deployment: Deploy using Helm
    Bug: T329530

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ type Config struct {
 	SystemUsers     []string `default:"system:serviceaccount:tekton-pipelines:tekton-pipelines-controller"`
 	AllowedBuilders []string `default:"paketobuildpacks/builder:base,gcr.io/buildpacks/builder:v1,docker-registry.tools.wmflabs.org/toolforge-bullseye0-builder:latest"`
 	Debug           bool     `default:"true"`
-	BuildID         string   `default:"nobuildid"`
 }
 
 func main() {
